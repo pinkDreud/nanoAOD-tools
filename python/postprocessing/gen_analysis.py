49ec846dfb41504e94a992fa746000dadd448bf8
Andrea Piccinelli <andrea8395@gmail.com>
2020-09-28 17:03:36 +0200

    W decays with genparticle countings

---
+++ python/postprocessing/gen_analysis.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import ROOT
+print("Succesfully imported ROOT")
+import math
+import datetime
+import copy
+from array import array
+from skimtree_utils import *
+
+startTime = datetime.datetime.now()
+print("Starting running at " + str(startTime))
+
+ROOT.gROOT.SetBatch()
+
+chain = ROOT.TChain('Events')
+print(chain)
+chain.Add("/eos/user/m/mmagheri/SampleVBS_nanoAOD/WpWp_EWK_2017_nanoAOD_file_prova.root")
+
+print("Number of events in chain " + str(chain.GetEntries()))
+print("Number of events in tree from chain " + str((chain.GetTree()).GetEntries()))
+print("Type of tree from chain " + str(type(chain.GetTree())))
+
+pdg_dict = {'1': ['d', 0],
+            '2': ['u', 0],
+            '3': ['s', 0],
+            '4': ['c', 0],
+            '5': ['b', 0],
+            '6': ['t', 0],
+            '7': ["b'", 0],
+            '8': ["t'", 0],
+            '11': ["ele", 0],
+            '12': ["nu_ele", 0],
+            '13': ["mu", 0],
+            '14': ["nu_mu", 0],
+            '15': ["tau", 0],
+            '16': ["nu_tau", 0],
+            '17': ["tau'", 0],
+            '18': ["nu_tau'", 0],
+        }
+        
+
+tree = InputTree(chain)
+print("Number of entries: " +str(tree.GetEntries()))
+print("tree: ", tree)
+
+lep = dict()
+quark = dict()
+d = dict()
+
+for i in range(tree.GetEntries()):
+   
+    event = Event(tree,i)
+    #electrons = Collection(event, "Electron")
+    #muons = Collection(event, "Muon")
+    #jets = Collection(event, "Jet")
+    #taus = Collection(event, "Tau")
+    #njets = len(jets)
+    genparts = Collection(event, "GenPart")
+
+    if i%1000==0: print("Processing event n. ---- "+str(i))
+
+    for genp in genparts:
+        if genp.genPartIdxMother > 0:
+            if abs(genparts[genp.genPartIdxMother].pdgId) == 24:
+                pdgid = str(abs(genp.pdgId))
+                if abs(int(pdgid)) < 20:
+                    pdg_dict[pdgid][1] += 1
+
+for pdgid, counts in pdg_dict.items():
+    if counts[1] != 0:
+        print counts[0], "\tcountings: ", counts[1]
