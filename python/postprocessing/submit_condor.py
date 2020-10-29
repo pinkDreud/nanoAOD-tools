45db59e12e1cc9db2a1872cf390f2dde36e9e59f
Andrea Piccinelli <andrea8395@gmail.com>
2020-10-29 12:17:06 +0100

    condor changes

--- python/postprocessing/submit_condor.py
+++ python/postprocessing/submit_condor.py
@@ -27,7 +27,7 @@ def sub_writer(sample, n, files, folder):
     f.write("use_x509userproxy       = true\n")
     f.write("should_transfer_files   = YES\n")
     f.write("when_to_transfer_output = ON_EXIT\n")
-    f.write("transfer_input_files    = $(Proxy_path), samples/samples.py, skimtree_utils.py, CutsAndValues_bu.py, __init__.py\n")
+    f.write("transfer_input_files    = $(Proxy_path), samples/samples.py, skimtree_utils.py, CutsAndValues.py, __init__.py\n")
     f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/user/"+inituser + "/" + username+"/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
     f.write("+JobFlavour             = \"workday\"\n") # options are espresso = 20 minutes, microcentury = 1 hour, longlunch = 2 hours, workday = 8 hours, tomorrow = 1 day, testmatch = 3 days, nextweek     = 1 week
     f.write("executable              = tree_skimmer_ssWW.py\n")
@@ -78,7 +78,6 @@ for sample in samples:
     print(str(len(files_list)))
     if(isMC):
         for i, files in enumerate(files_list):
-            if i > 9: break
             sub_writer(sample, i, files, folder)
             os.popen('condor_submit condor.sub')
             print('condor_submit condor.sub')
