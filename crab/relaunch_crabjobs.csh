dfdd37fbb80e84d09f5fd3e96f069afc78803829
Andrea Piccinelli <andrea8395@gmail.com>
2020-10-30 15:47:21 +0100

    readSelection upgraded version

--- crab/relaunch_crabjobs.csh
+++ crab/relaunch_crabjobs.csh
@@ -2,7 +2,7 @@ set year = '2017'
 python submit_crab.py -d TT_SemiLep$year -r
 #python submit_crab.py -d TT_Mtt_$year -r
 #python submit_crab.py -d WJetsHT100to200_$year -r
-python submit_crab.py -d WJetsHT600to800_$year -r
+#python submit_crab.py -d WJetsHT600to800_$year -r
 #python submit_crab.py -d WZ_$year -r
 python submit_crab.py -d DY2JetsToLL_$year -r
 #python submit_crab.py -d WpWpJJ_EWK_$year -r
--- python/postprocessing/readSelection.py
+++ python/postprocessing/readSelection.py
@@ -1,12 +1,73 @@
 from skimtree_utils import *
 import ROOT
+import os
+import optparse
+import sys
+from samples.samples import *
 
-infile = "/eos/user/a/apiccine/VBS/nosynch/tageff/WpWpJJ_EWK_2017/WpWpJJ_EWK_2017_part4.root"
-chain = ROOT.TChain('events_all')
-chain.Add(infile)
-tree = InputTree(chain)
+usage = 'python SetAndLaunchCondorRun.py -y year'
+parser = optparse.OptionParser(usage)
+parser.add_option('-y', dest='year', type=str, default = '2017', help='Please enter a year, default is 2017')
 
-for i in range(tree.GetEntries()):
-    event       = Event(tree,i)
-    passed = Object(event, "pass")#cosi carico tutti i pass_* in unico object
-    print passed.lepton_selection# cosi ottengo pass_lepton_selection
+(opt, args) = parser.parse_args()
+
+authors_dict = {'a': 'apiccine',
+                'm': 'mmagheri',
+}
+
+wpset_dict = {'a': [('M', 'VL', 'VVL'),
+                    ('T', 'VL', 'VVL'),
+                    ('VT', 'VL', 'VVL'),
+                    ('M', 'L', 'VVL'),
+                    ('T', 'L', 'VVL'),
+                    ('VT', 'L', 'VVL'),
+                    ('M', 'VL', 'VL'),
+                    ('T', 'VL', 'VL'),
+                    ('VT', 'VL', 'VL'),
+                    ],
+              'm': [('M', 'L', 'VL'),
+                    ('T', 'L', 'VL'),
+                    ('VT', 'L', 'VL'),
+                    ('M', 'VL', 'L'),
+                    ('T', 'VL', 'L'),
+                    ('VT', 'VL', 'L'),
+                    ('M', 'L', 'L'),
+                    ('T', 'L', 'L'),
+                    ('VT', 'L', 'L'),
+                    ],
+}
+
+#username = str(os.environ.get('USER'))
+#inituser = str(os.environ.get('USER')[0])
+
+for aut, wpconfs in wpset_dict.items():
+    #carica autore e wp config
+    for wpconf in wpconfs:
+        #scrive la path a seconda di chi ha prodotto una certa wp config
+        path = "/eos/user/" + aut + "/" + authors_dict[aut] + "/VBS/nosynch/Eff_Jet" + wpconf[0] + "_Mu" + wpconf[1] + "_Ele" + wpconf[2] + "/"
+
+        #se non esiste, va avanti
+        if not os.path.exists(path):
+            continue
+
+        #carica le cartelle dei sample
+        dirlist = [dirs for dirs in os.listdir(path)]
+
+        for dirn in dirlist:
+            #carica il _merged.root
+            mergedfile = path + dirn + "/" + dirn + "_merged.root"
+
+            #carica il tree events_all
+            chain = ROOT.TChain('events_all')
+            chain.Add(mergedfile)
+
+            print chain
+            
+            tree = InputTree(chain)
+            print tree.GetEntries()
+            
+            for i in range(tree.GetEntries()):
+                event       = Event(tree,i)
+                passed = Object(event, "pass")#cosi carico tutti i pass_* in unico object
+                #print passed.lepton_selection# cosi ottengo pass_lepton_selection
+
