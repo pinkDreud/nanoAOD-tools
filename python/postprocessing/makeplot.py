eb31020233b8644af36369fbaa429c22e7be8396
pinkDreud <matteo.magherini@gmail.com>
2020-11-20 10:05:40 +0100

    Tranquillo github, andra' tutto bene

--- python/postprocessing/makeplot.py
+++ python/postprocessing/makeplot.py
@@ -450,8 +450,7 @@ def makestack(lep_, reg_, variabile_, samples_, cut_tag_, syst_, lumi):
           infile[s.label].Delete()
      os.system('set LD_PRELOAD=libtcmalloc.so')
 
-#dataset_dict = {'2016':[],'2017':[],'2018':[]}
-dataset_dict = {'2017':[],'2018':[]}
+dataset_dict = {'2016':[],'2017':[],'2018':[]}
 
 if(opt.dat!= 'all'):
      if not(opt.dat in sample_dict.keys()):
@@ -464,7 +463,7 @@ if(opt.dat!= 'all'):
 else:
      dataset_dict = {
           #'2016':[TT_2016, WJets_2016, WZ_2016, DYJetsToLL_2016, WpWpJJ_EWK_2016, WpWpJJ_QCD_2016],#[DataMu_2016, DataEle_2016, DataHT_2016],
-          '2017':[WpWpJJ_QCD_2017, WZ_2017, TT_2017, DYJetsToLL_2017, WJets_2017, WpWpJJ_EWK_2017, DataMu_2017],# DataEle_2017, DataHT_2017],
+          '2017':[WpWpJJ_QCD_2017, WZ_2017, TT_2017, DYJetsToLL_2017, WJets_2017, WpWpJJ_EWK_2017, DataMu_2017, DataEle_2017, DataHT_2017],
           '2018':[TT_2018, WpWpJJ_QCD_2018, WZ_2018, DYJetsToLL_2018, WJets_2018, WpWpJJ_EWK_2018], #[DataMu_2017, DataEle_2017, DataHT_2017],
      }
 #print(dataset_dict.keys())
