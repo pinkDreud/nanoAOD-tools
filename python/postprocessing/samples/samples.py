03fe9679a36eabf2b21df6e75ad6cae895d05388
Andrea Piccinelli <andrea8395@gmail.com>
2020-12-04 16:02:09 +0100

    ZZ added

--- python/postprocessing/samples/samples.py
+++ python/postprocessing/samples/samples.py
@@ -209,39 +209,39 @@ ZZJJTo4L_QCD_2017.dataset = "/ZZJJTo4L_QCD_TuneCP5_13TeV-madgraph-pythia8/" + ta
 ZZJJTo4L_QCD_2017.sigma = 0.008348 #pb
 
 GluGluToContinToZZTo2e2nu_2017 = sample(ROOT.kGray+2, 1, 1001, "ZZ", "GluGluToContinToZZTo2e2nu")
-GluGluToContinToZZTo2e2nu_QCD_2017.year = 2017
-GluGluToContinToZZTo2e2nu_QCD_2017.dataset = "/GluGluToContinToZZTo2e2nu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
-GluGluToContinToZZTo2e2nu_QCD_2017.sigma = 17.72 #pb
+GluGluToContinToZZTo2e2nu_2017.year = 2017
+GluGluToContinToZZTo2e2nu_2017.dataset = "/GluGluToContinToZZTo2e2nu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
+GluGluToContinToZZTo2e2nu_2017.sigma = 17.72 #pb
 
 GluGluToContinToZZTo2e2mu_2017 = sample(ROOT.kGray+2, 1, 1001, "ZZ", "GluGluToContinToZZTo2e2mu")
-GluGluToContinToZZTo2e2mu_QCD_2017.year = 2017
-GluGluToContinToZZTo2e2mu_QCD_2017.dataset = "/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
-GluGluToContinToZZTo2e2mu_QCD_2017.sigma = 3.291 #pb
+GluGluToContinToZZTo2e2mu_2017.year = 2017
+GluGluToContinToZZTo2e2mu_2017.dataset = "/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
+GluGluToContinToZZTo2e2mu_2017.sigma = 3.291 #pb
 
 GluGluToContinToZZTo2e2tau_2017 = sample(ROOT.kGray+2, 1, 1001, "ZZ", "GluGluToContinToZZTo2e2tau")
-GluGluToContinToZZTo2e2tau_QCD_2017.year = 2017
-GluGluToContinToZZTo2e2tau_QCD_2017.dataset = "/GluGluToContinToZZTo2e2tau_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
-GluGluToContinToZZTo2e2tau_QCD_2017.sigma = 3.29 #pb
+GluGluToContinToZZTo2e2tau_2017.year = 2017
+GluGluToContinToZZTo2e2tau_2017.dataset = "/GluGluToContinToZZTo2e2tau_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
+GluGluToContinToZZTo2e2tau_2017.sigma = 3.29 #pb
 
 GluGluToContinToZZTo2mu2nu_2017 = sample(ROOT.kGray+2, 1, 1001, "ZZ", "GluGluToContinToZZTo2mu2nu")
-GluGluToContinToZZTo2mu2nu_QCD_2017.year = 2017
-GluGluToContinToZZTo2mu2nu_QCD_2017.dataset = "/GluGluToContinToZZTo2mu2nu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
-GluGluToContinToZZTo2mu2nu_QCD_2017.sigma = 17.72 #pb
+GluGluToContinToZZTo2mu2nu_2017.year = 2017
+GluGluToContinToZZTo2mu2nu_2017.dataset = "/GluGluToContinToZZTo2mu2nu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
+GluGluToContinToZZTo2mu2nu_2017.sigma = 17.72 #pb
 
 GluGluToContinToZZTo2mu2tau_2017 = sample(ROOT.kGray+2, 1, 1001, "ZZ", "GluGluToContinToZZTo2mu2tau")
-GluGluToContinToZZTo2mu2tau_QCD_2017.year = 2017
-GluGluToContinToZZTo2mu2tau_QCD_2017.dataset = "/GluGluToContinToZZTo2mu2tau_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
-GluGluToContinToZZTo2mu2tau_QCD_2017.sigma = 3.289 #pb
+GluGluToContinToZZTo2mu2tau_2017.year = 2017
+GluGluToContinToZZTo2mu2tau_2017.dataset = "/GluGluToContinToZZTo2mu2tau_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
+GluGluToContinToZZTo2mu2tau_2017.sigma = 3.289 #pb
 
 GluGluToContinToZZTo4e_2017 = sample(ROOT.kGray+2, 1, 1001, "ZZ", "GluGluToContinToZZTo4e")
-GluGluToContinToZZTo4e_QCD_2017.year = 2017
-GluGluToContinToZZTo4e_QCD_2017.dataset = "/GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
-GluGluToContinToZZTo4e_QCD_2017.sigma = 1.405 #pb
+GluGluToContinToZZTo4e_2017.year = 2017
+GluGluToContinToZZTo4e_2017.dataset = "/GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
+GluGluToContinToZZTo4e_2017.sigma = 1.405 #pb
 
 GluGluToContinToZZTo4mu_2017 = sample(ROOT.kGray+2, 1, 1001, "ZZ", "GluGluToContinToZZTo4mu")
-GluGluToContinToZZTo4mu_QCD_2017.year = 2017
-GluGluToContinToZZTo4mu_QCD_2017.dataset = "/GluGluToContinToZZTo4mu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
-GluGluToContinToZZTo4mu_QCD_2017.sigma = 1.402 #pb
+GluGluToContinToZZTo4mu_2017.year = 2017
+GluGluToContinToZZTo4mu_2017.dataset = "/GluGluToContinToZZTo4mu_13TeV_MCFM701_pythia8/" + tag3_2017 + "-v1/NANOAODSIM"
+GluGluToContinToZZTo4mu_2017.sigma = 1.402 #pb
 
 ZZ_2017 = sample(ROOT.kGray, 1, 1001, "ZZ", "ZZ_2017")
 ZZ_2017.year = 2017
