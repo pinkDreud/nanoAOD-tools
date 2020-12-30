d20c80cc0e351ecbe301112f09753d3a446ae901
Andrea Piccinelli <andrea8395@gmail.com>
2020-12-30 18:51:46 +0100

    fileswriter.py updated

--- crab/launch_crabjobs.csh
+++ crab/launch_crabjobs.csh
@@ -18,9 +18,9 @@ set year = '2017'
 #python submit_crab.py -d Other_$year -s
 #python submit_crab.py -d WpWpJJ_EWK_$year -s
 #python submit_crab.py -d WpWpJJ_QCD_$year -s
-python submit_crab.py -d DataHT_$year -s
+#python submit_crab.py -d DataHT_$year -s
 #python submit_crab.py -d QCD_$year -s
-#python submit_crab.py -d DataEle_$year -s
-#python submit_crab.py -d DataMu_$year -s
+python submit_crab.py -d DataEle_$year -s
+python submit_crab.py -d DataMu_$year -s
 #python submit_crab.py -d DataMET_$year -s
 
--- crab/macros/files_writer.py
+++ crab/macros/files_writer.py
@@ -25,24 +25,52 @@ for sample in samples:
     if not os.path.exists("./files/"):
         os.makedirs("./files/")
     f = open("./files/"+str(sample.label)+".txt", "w")
-    url = os.popen('crab getoutput --xrootd --jobids=1 -d ' + path + '/crab_' + str(sample.label) + '/').readlines()[0]
-    print(url)
+    url = os.popen('crab getoutput --xrootd --quantity="all" -d ' + path + '/crab_' + str(sample.label) + '/').readlines()#[0]
+    for u in url:
+        #f.write
+        if 'root' not in u:
+            npaths = int(u.split("for ")[-1].split(" ")[0])
+
+            finished = False
+            rang = 500
+            t = 0
+            while not finished:
+                intmin = int(t*500+1)
+                intmax = int(min((t+1)*500, npaths))
+                crabgo = str(intmin)+'-'+str(intmax)
+                curl = os.popen('crab getoutput --xrootd --jobids=' + str(crabgo) + ' -d ' + path + '/crab_' + str(sample.label) + '/').readlines()
+                for cu in curl:
+                    f.write(cu)
+                if intmax == npaths:
+                    finished = True
+                t+=1
+                
+            break
+        else:
+            f.write(u)
+f.close()
+
+'''
     s1=url.split(str(os.environ.get('USER')))
     print(s1[1])
     s2=s1[1].split('0000')
-
+    
     path_xrd = 'root://cms-xrd-global.cern.ch//store/user/' + str(os.environ.get('USER')) + s2[0]
     newurl = 'srm://stormfe1.pi.infn.it/cms/store/user/' + str(os.environ.get('USER')) + s2[0]
     #path_xrd = 'root://cms-xrd-global.cern.ch//store/user/adeiorio/OutDir/JetHT/DataHTB_2016/200511_122826/'
     #newurl = 'srm://stormfe1.pi.infn.it/cms/store/user/adeiorio/OutDir/JetHT/DataHTB_2016/200511_122826/'
 
-    print(newurl)
-    
+    #newurl = path_xrd
+    #print(newurl)
+
+
     i=0
     print('\nChecking files in the folder '+newurl.strip('\n')+'\n')
     while True:
         folder = os.popen('eval `scram unsetenv -sh`; gfal-ls '+ newurl.strip('\n')+'000'+str(i)).readlines()
+        print(folder)
         newpath_xrd = path_xrd.strip('\n')+'000'+str(i)
+
         if(len(folder)==0):
             print("The folder does not exist: "+ str(folder))
             break
@@ -60,3 +88,4 @@ for sample in samples:
     #print('Setting cmsenv again')
     #os.popen("eval `scram runtime -csh`")
     print('Goodbye')
+'''
