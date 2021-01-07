ca0be7a556c4ef5e2202764793b251481d741d35
Andrea Piccinelli <andrea8395@gmail.com>
2021-01-07 12:14:48 +0100

    tree_skimmer v20 updated

--- python/postprocessing/tree_skimmer_ssWW_wFakes.py
+++ python/postprocessing/tree_skimmer_ssWW_wFakes.py
@@ -18,7 +18,7 @@ import math
 import datetime
 import copy
 from array import array
-from skimtree_utils_ssWW_wFakes_dev import *
+from skimtree_utils_ssWW_wFakes import *
 
 usage = "python tree_skimmer_ssWW_wFakes.py [nome_del_sample_in_samples.py] 0 [file_in_input] local"
 
