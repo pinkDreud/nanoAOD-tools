5151fdb6ef3ce706e88cafb13cc31ed4133bc8e6
Andrea Piccinelli <andrea8395@gmail.com>
2021-01-28 10:43:46 +0100

    submit_condor_FR reloaded

--- python/postprocessing/submit_condor_FR_dev.py
+++ python/postprocessing/submit_condor_FR_dev.py
@@ -34,7 +34,7 @@ def sub_writer(sample, n, files, folder):
     f.write("use_x509userproxy       = true\n")
     f.write("should_transfer_files   = YES\n")
     f.write("when_to_transfer_output = ON_EXIT\n")
-    f.write("transfer_input_files    = $(Proxy_path), samples/samples.py, FakeRatio_utils_dev.py, CutsAndValues_bu.py, __init__.py\n")
+    f.write("transfer_input_files    = $(Proxy_path), TH2F_FakeRatio_ele.root, TH2F_FakeRatio_mu.root, samples/samples.py, FakeRatio_utils_dev.py, CutsAndValues_bu.py, __init__.py\n")
     f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/home-"+inituser + "/" + username+"/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
     f.write("+JobFlavour             = \"workday\"\n") # options are espresso = 20 minutes, microcentury = 1 hour, longlunch = 2 hours, workday = 8 hours, tomorrow = 1 day, testmatch = 3 days, nextweek     = 1 week
     f.write("executable              = FakeRatio_dev.py\n")
