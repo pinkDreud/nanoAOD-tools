1b431476a86f3d42d550fec77b25ad072c6a6534
apiccine <andrea.piccinelli@cern.ch>
2021-04-07 17:51:59 +0200

    crab modules updated

--- python/postprocessing/modules/common/muonScaleResProducer.py
+++ python/postprocessing/modules/common/muonScaleResProducer.py
@@ -1,10 +1,9 @@
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 import ROOT
 import os
 import random
 ROOT.PyConfig.IgnoreCommandLineOptions = True
-
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
-from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 
 def mk_safe(fct, *args):
@@ -12,7 +11,7 @@ def mk_safe(fct, *args):
         return fct(*args)
     except Exception as e:
         if any('Error in function boost::math::erf_inv' in arg for arg in e.args):
-            print 'WARNING: catching exception and returning -1. Exception arguments: %s' % e.args
+            print('WARNING: catching exception and returning -1. Exception arguments: %s' % e.args)
             return -1.
         else:
             raise e
@@ -24,7 +23,7 @@ class muonScaleResProducer(Module):
         p_roccor = p_postproc + '/data/' + rc_dir
         if "/RoccoR_cc.so" not in ROOT.gSystem.GetLibraries():
             p_helper = '%s/RoccoR.cc' % p_roccor
-            print 'Loading C++ helper from ' + p_helper
+            print('Loading C++ helper from ' + p_helper)
             ROOT.gROOT.ProcessLine('.L ' + p_helper)
         self._roccor = ROOT.RoccoR(p_roccor + '/' + rc_corrections)
 
@@ -50,39 +49,31 @@ class muonScaleResProducer(Module):
             genparticles = Collection(event, "GenPart")
         roccor = self._roccor
         if self.is_mc:
-            pt_corr=[]
-            pt_err=[]
+            pt_corr = []
+            pt_err = []
             for mu in muons:
                 genIdx = mu.genPartIdx
                 if genIdx >= 0 and genIdx < len(genparticles):
                     genMu = genparticles[genIdx]
                     pt_corr.append(mu.pt * mk_safe(roccor.kSpreadMC, mu.charge, mu.pt, mu.eta, mu.phi, genMu.pt))
-                    pt_err.append(mu.pt*mk_safe(roccor.kSpreadMCerror, mu.charge, mu.pt, mu.eta, mu.phi, genMu.pt))
+                    pt_err.append(mu.pt *  mk_safe(roccor.kSpreadMCerror, mu.charge, mu.pt, mu.eta, mu.phi, genMu.pt))
                 else:
                     u1 = random.uniform(0.0, 1.0)
-                    pt_corr.append(mu.pt*mk_safe(roccor.kSmearMC, mu.charge, mu.pt, mu.eta, mu.phi, mu.nTrackerLayers, u1))
-                    pt_err.append(mu.pt*mk_safe(roccor.kSmearMCerror, mu.charge, mu.pt, mu.eta, mu.phi, mu.nTrackerLayers, u1))
+                    pt_corr.append(mu.pt * mk_safe(roccor.kSmearMC, mu.charge, mu.pt, mu.eta, mu.phi, mu.nTrackerLayers, u1))
+                    pt_err.append(mu.pt * mk_safe(roccor.kSmearMCerror, mu.charge, mu.pt, mu.eta, mu.phi, mu.nTrackerLayers, u1))
 
         else:
-            pt_corr = list(
-                mu.pt * mk_safe(
-                    roccor.kScaleDT,
-                    mu.charge, mu.pt, mu.eta, mu.phi
-                    ) for mu in muons)
-            pt_err = list(
-                mu.pt * mk_safe(
-                    roccor.kScaleDTerror,
-                    mu.charge, mu.pt, mu.eta, mu.phi
-                    ) for mu in muons)
+            pt_corr = list(mu.pt * mk_safe(roccor.kScaleDT, mu.charge, mu.pt, mu.eta, mu.phi) for mu in muons)
+            pt_err = list(mu.pt * mk_safe(roccor.kScaleDTerror, mu.charge, mu.pt, mu.eta, mu.phi) for mu in muons)
 
         self.out.fillBranch("Muon_corrected_pt", pt_corr)
-        pt_corr_up = list( max(pt_corr[imu]+pt_err[imu], 0.0) for imu,mu in enumerate(muons) )
-        pt_corr_down = list( max(pt_corr[imu]-pt_err[imu], 0.0) for imu,mu in enumerate(muons) )
-        self.out.fillBranch("Muon_correctedUp_pt",  pt_corr_up)
-        self.out.fillBranch("Muon_correctedDown_pt",  pt_corr_down)
+        pt_corr_up = list(max(pt_corr[imu] + pt_err[imu], 0.0) for imu, mu in enumerate(muons))
+        pt_corr_down = list(max(pt_corr[imu] - pt_err[imu], 0.0) for imu, mu in enumerate(muons))
+        self.out.fillBranch("Muon_correctedUp_pt", pt_corr_up)
+        self.out.fillBranch("Muon_correctedDown_pt", pt_corr_down)
         return True
 
 
-muonScaleRes2016 = lambda : muonScaleResProducer('roccor.Run2.v3', 'RoccoR2016.txt', 2016)
-muonScaleRes2017 = lambda : muonScaleResProducer('roccor.Run2.v3', 'RoccoR2017.txt', 2017)
-muonScaleRes2018 = lambda : muonScaleResProducer('roccor.Run2.v3', 'RoccoR2018.txt', 2018)
+muonScaleRes2016 = lambda: muonScaleResProducer('roccor.Run2.v3', 'RoccoR2016.txt', 2016)
+muonScaleRes2017 = lambda: muonScaleResProducer('roccor.Run2.v3', 'RoccoR2017.txt', 2017)
+muonScaleRes2018 = lambda: muonScaleResProducer('roccor.Run2.v3', 'RoccoR2018.txt', 2018)
