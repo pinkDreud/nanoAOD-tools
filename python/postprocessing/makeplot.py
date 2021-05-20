cc2643a984b6d4fd1d80f03685b64f7b241bf53d
apiccine <andrea.piccinelli@cern.ch>
2021-05-20 19:49:36 +0200

    update

--- python/postprocessing/makeplot.py
+++ python/postprocessing/makeplot.py
@@ -825,16 +825,6 @@ else:
                if 'DataEle' in v.label or 'FakeEle' in v.label or 'PromptEle' in v.label:
                     continue
           dataset_dict[str(v.year)].append(v)
-     '''
-          '2017':[WpWpJJ_QCD_2017, WZ_2017, TT_2017, DYJetsToLL_2017, WJets_2017, WpWpJJ_EWK_2017, DataMu_2017, DataEle_2017],#, DataHT_2017],
-          #'2016':[TT_2016, WJets_2016, WZ_2016, DYJetsToLL_2016, WpWpJJ_EWK_2016, WpWpJJ_QCD_2016],#[DataMu_2016, DataEle_2016, DataHT_2016],
-          '2018':[TT_2018, WpWpJJ_QCD_2018, WZ_2018, DYJetsToLL_2018, WJets_2018, WpWpJJ_EWK_2018], #[DataMu_2017, DataEle_2017, DataHT_2017],
-     }
-
-
-          '2016':[TT_2016, WJets_2016, WZ_2016, DYJetsToLL_2016, WpWpJJ_EWK_2016, WpWpJJ_QCD_2016],#[DataMu_2016, DataEle_2016, DataHT_2016],
-          '2018':[TT_2018, WpWpJJ_QCD_2018, WZ_2018, DYJetsToLL_2018, WJets_2018, WpWpJJ_EWK_2018, #[DataMu_2017, DataEle_2017, DataHT_2017],
-     '''
 
 for v in dataset_dict.values():
      print([o.label for o in v])
